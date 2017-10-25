7a5c30b6c513ec0c4dd02032e85578fb53f14ae9
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-25 20:11:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n;
+  }
+}
+
+function deepestChild() {
+  let deepBoys = document.querySelectorAll('#grand-node div');
+
+  return deepBoys[deepBoys.length - 1];
+}
